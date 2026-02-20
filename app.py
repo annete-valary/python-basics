9919d0d37902ffd525d7b8dce2dec531824d5ac2
dennisthinwa93 <dennisthinwa93@gmail.com>
2026-02-20 16:41:58 +0300

    python notes

---
+++ app.py
@@ -0,0 +1,120 @@
+#this is a comment
+"""
+this is a multiline string
+but can also be used as a multiline comment
+"""
+import webbrowser
+
+#VARIABLES
+weight=20
+name="John Doe"
+name="Mark Lou"
+weight=20.5
+output=""
+
+
+"""DATA TYPES:
+   -NUMBERS:integers,float,complex
+   -TEXT:strings str()
+   -LIST:list ,set,tuple,dictionary
+"""
+#integer: int()
+numberOne=20
+output=type(numberOne) #checking data type
+output=numberOne
+""" ARITHMETIC OPERATORS:
+   -ADD:sum
+   -addition +
+   -subtraction-
+   -multiplication*
+   -power()
+   -floor division
+   -modulus%
+
+
+"""
+x=10
+y=12
+output=x+y
+output=x-y
+output=x*y
+output=x/y
+output=x//y
+output=x%y
+x=10
+# x+=1
+# x-=1
+# x=x*3
+# x/=9
+# x%=3
+# x**=3
+"""
+COMPARISON OPERATORS:
+#     less than <
+#  greater than >
+# less than or equal to <=
+# greater than or equal to >=
+# 
+"""
+x=100
+y=90
+output=x<y
+output=x>y
+output=x>=y
+output=x<=y
+output=x>y
+
+output=x!=y
+"""BOOLEAN OPERATORS:
+   -and
+   -or
+   -not//
+   """
+fruit="orange"
+weather="sunny"
+output=(fruit=="orange") and (weather=="rainy")
+
+# age=12
+# weight=20.5
+
+# output=(age>18) and (weight>5)
+# output=(age<18) or (weight<5)
+
+btemp=20
+rtemp=30
+output=(btemp<30) and (rtemp>20)
+
+"""
+STRINGS:
+# list of characters2
+# surrounded by either'' ,"" , """ """
+# immutable
+# 
+
+"""
+name="John Doe"
+output=name
+output=name [1:8:4] # slicing using step (It prints the letters after every four characters in the eight characters of JOHN DOE)
+NAME="PURPLE HIBISCUS"
+output=NAME [1:15:4]
+
+
+name="Purple hibiscus"
+# output=name.upper()
+output=name.find("purple ")
+output=name[14:]
+output=name.count("i")
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(output)
